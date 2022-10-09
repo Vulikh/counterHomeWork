c25fe585d5a9588f80ea56357302b1f76c5880f9
Vulikh <fed.vul@icloud.com>
2022-10-09 19:14:13 +0300

    Создано приложение "Счетчик нажатий"

--- counterHomeWork/ViewController.swift
+++ counterHomeWork/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterTextLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    var counts: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap() {
+        counts += 1
+        counterTextLabel.text = "Значение счетчика: \(counts)"
+    }
+    
 }
 
